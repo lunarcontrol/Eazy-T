71a939ab6cb464dfa551b4f4c70181316eb1d909
Royce Whitaker <roycewhitaker@rocketmail.com>
2018-07-01 15:28:22 -0700

    Add files via upload

---
+++ sw-old.js
@@ -0,0 +1,43 @@
+// var CACHE_NAME = 'version-one';
+// var urlsToCache = [
+//   './index.html',
+//   './about.html',
+//   './settings.html',
+//   './css/bars.css',
+//   './css/index.css',
+//   './js/alertify.js'
+// ];
+//v1.0.2
+
+self.addEventListener('install', function(event) {
+  // We pass a promise to event.waitUntil to signal how 
+  // long install takes, and if it failed
+  event.waitUntil(
+    // We open a cache…
+    caches.open('simple-sw-v1').then(function(cache) {
+      // And add resources to it
+      return cache.addAll([
+        './'
+      ]);
+    })
+  );
+});
+
+// The fetch event happens for the page request with the
+// ServiceWorker's scope, and any request made within that
+// page
+self.addEventListener('fetch', function(event) {
+  // Calling event.respondWith means we're in charge
+  // of providing the response. We pass in a promise
+  // that resolves with a response object
+  event.respondWith(
+    // First we look for something in the caches that
+    // matches the request
+    caches.match(event.request).then(function(response) {
+      // If we get something, we return it, otherwise
+      // it's null, and we'll pass the request to
+      // fetch, which will use the network.
+      return response || fetch(event.request);
+    })
+  );
+});
